aabec16dc624a6a53082f2cf520f7af97339fb73
jav-26 <63278159+jav-26@users.noreply.github.com>
2020-06-22 01:58:14 -0500

    Agregando lo borrado

--- src/main/java/com/cong/predictiveText/DataNormalizer.java
+++ src/main/java/com/cong/predictiveText/DataNormalizer.java
@@ -5,6 +5,16 @@ import java.util.regex.*;
 
 public class DataNormalizer {
 
+    public static Set<String> getListWord(String document){
+        Set<String> listWord = new HashSet<String>();
+
+        Pattern pattern = Pattern.compile("<post>(.*?)</post>", Pattern.DOTALL);
+        Matcher matcher = pattern.matcher(document);
+        while (matcher.find()){
+            listWord.addAll(DataNormalizer.splitWord(matcher.group(1).trim()));
+        }
+        return listWord;
+    }
 
     public static Set<String> splitWord(String line){
         line = line.replaceAll("[!?,.:;`'~@*#$%&()|\"]", " ");
